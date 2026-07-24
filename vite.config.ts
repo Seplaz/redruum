0d230fc0370efa05aece86c5b789e801bc58025b
Mikhail Karasev <seplaz38@yandex.ru>
2026-07-24 12:44:39 +0800

    style: comment out build configuration in vite.config.ts for future reference

--- vite.config.ts
+++ vite.config.ts
@@ -5,33 +5,33 @@ import { visualizer } from 'rollup-plugin-visualizer';
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/redruum/',
-  build: {
-    chunkSizeWarningLimit: 800,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react-dom') || id.includes('react')) {
-              return 'react-vendor';
-            }
-            if (id.includes('@supabase')) {
-              return 'supabase-vendor';
-            }
-            if (id.includes('motion')) {
-              return 'motion-vendor';
-            }
-            if (
-              id.includes('@vercel/analytics') ||
-              id.includes('@vercel/speed-insights')
-            ) {
-              return 'vercel-vendor';
-            }
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   chunkSizeWarningLimit: 800,
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           if (id.includes('react-dom') || id.includes('react')) {
+  //             return 'react-vendor';
+  //           }
+  //           if (id.includes('@supabase')) {
+  //             return 'supabase-vendor';
+  //           }
+  //           if (id.includes('motion')) {
+  //             return 'motion-vendor';
+  //           }
+  //           if (
+  //             id.includes('@vercel/analytics') ||
+  //             id.includes('@vercel/speed-insights')
+  //           ) {
+  //             return 'vercel-vendor';
+  //           }
+  //           return 'vendor';
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
   plugins: [
     react(),
     visualizer({
